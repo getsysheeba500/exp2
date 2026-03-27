28421402a2bb7013bb79943497aa92ba5b3836e0
getsysheeba500 <juug24bcas38854@jainuniversity.ac.in>
2026-03-27 09:39:37 +0530

    Add files via upload

---
+++ TableActivity.java
@@ -0,0 +1,16 @@
+package com.example.layoutexp6;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TableActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitytable);
+    }
+}
